39bc57e7d4aa6936839dc07df4bd3b73b3dfb257
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2023-03-31 16:16:07 +0000

    Deploying to gh-pages from @ viktormashyka/movie-finder-api@8bbc005b3923d9329ae982e59ef5f391dc339155 🚀

--- src/js/feach/const.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = 'ba12bbb2efd4020faab2c5dd14dc19c0';
--- src/js/loader.js
+++
@@ -1,7 +0,0 @@
-export default function loader() {
-  const loader = document.getElementById('preloader');
-  loader.style.display = 'flex';
-  setTimeout(() => {
-    loader.style.display = 'none';
-  }, 800);
-}
